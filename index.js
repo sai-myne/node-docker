5e71564cf6fae6f8debe2b40f45f5c0a8e9c44ba
sai-myne <ahlusir@gmail.com>
2021-08-03 15:08:48 +0800

    Communicating between containers

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("<h2>Hi There</h2>")
-})
+mongoose
+  .connect(
+    "mongodb://sainonemyne:dfasdfasdfasd@mongo:27017/?authSource=admin"
+  )
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
 
-const port = process.env.PORT || 3000
+app.get("/", (req, res) => {
+  res.send("<h2>Hi There</h2>");
+});
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
