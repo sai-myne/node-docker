f90839579b3f1d62deb352b38687870e34fb7795
sai-myne <ahlusir@gmail.com>
2021-08-03 16:26:30 +0800

    Sign up and Login

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 } = require("./config/config");
 
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes")
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -33,6 +34,7 @@ app.get("/", (req, res) => {
 });
 //localhost:3000/api/v1/post/
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
