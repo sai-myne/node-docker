a7b449e51e7a3d8056d1f9035d168aacab53aa8d
sai-myne <ahlusir@gmail.com>
2021-08-04 10:21:31 +0800

    Express CORS

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const session = require('express-session');
-const redis = require('redis');
-let RedisStore = require('connect-redis')(session);
+const session = require("express-session");
+const redis = require("redis");
+let RedisStore = require("connect-redis")(session);
+const cors = require("cors");
 
 const {
   MONGO_USER,
@@ -17,10 +18,10 @@ const {
 let redisClient = redis.createClient({
   host: REDIS_URL,
   port: REDIS_PORT,
-})
+});
 
 const postRouter = require("./routes/postRoutes");
-const userRouter = require("./routes/userRoutes")
+const userRouter = require("./routes/userRoutes");
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -40,24 +41,30 @@ const connectWithRetry = () => {
 };
 
 connectWithRetry();
-app.enable("trust proxy")
-app.use(session({
-  store: new RedisStore({client: redisClient}),
-  secret: SESSION_SECRET,
-  cookie: {
-    secure: false,
-    resave: false,
-    saveUninitialized: false,
-    httpOnly: true,
-    maxAge: 30000,
-  },
-}))
+app.enable("trust proxy");
+app.use(cors({}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
 
 app.use(express.json());
 app.get("/api/v1", (req, res) => {
   res.send("<h2>Hi There</h2>");
-  console.log("yeah it ran")
+  console.log("yeah it ran");
 });
+
+//www.google.com -> www.yahoo.com
+
 //localhost:3000/api/v1/post/
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/users", userRouter);
