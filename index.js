5500f6f25b10b1cab3abe2326601d25c9b403a94
sai-myne <ahlusir@gmail.com>
2021-08-04 09:38:01 +0800

    Authentication with sessions & Redis

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const redis = require('redis');
+let RedisStore = require('connect-redis')(session);
+
 const {
   MONGO_USER,
   MONGO_PORT,
   MONGO_PASSWORD,
   MONGO_IP,
+  REDIS_URL,
+  SESSION_SECRET,
+  REDIS_PORT,
 } = require("./config/config");
+
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+})
 
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes")
@@ -28,6 +40,19 @@ const connectWithRetry = () => {
 };
 
 connectWithRetry();
+
+app.use(session({
+  store: new RedisStore({client: redisClient}),
+  secret: SESSION_SECRET,
+  cookie: {
+    secure: false,
+    resave: false,
+    saveUninitialized: false,
+    httpOnly: true,
+    maxAge: 30000,
+  },
+}))
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h2>Hi There</h2>");
